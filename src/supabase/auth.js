6fbfa0d5336a0b973c963491ef0aab965b566656
Pouya Khoshnavazi <pouyanavazi@gmail.com>
2025-10-04 15:37:54 -0700

    Added template for user authentication, supabase db is setup.

---
+++ src/supabase/auth.js
@@ -0,0 +1,17 @@
+/*
+Module responsible for functions that authenticate user given email and password
+*/
+
+import client from "./client"
+
+// REQUIRES: password as a string, email as a string 
+// EFFECTS: adds user to supabase 
+function signUp(password, email) {
+    
+
+}
+
+function login() {
+
+}
+
