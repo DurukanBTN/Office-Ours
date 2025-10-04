18d0082bbdc9aec51b552df9286b83b2cb6ae23e
Pouya Khoshnavazi <pouyanavazi@gmail.com>
2025-10-04 14:54:12 -0700

    Created Supabase client for usage throughout modules.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import client from "./supabase/client"
 
 function App() {
+    console.log(client);
   const [count, setCount] = useState(0)
 
   return (
