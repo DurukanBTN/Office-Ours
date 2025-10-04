67fbfbc444e8f893477ef8ed706635c5f27fa1c2
ihuang05 <ihuang05@students.cs.ubc.ca>
2025-10-04 16:07:28 -0700

    Changed some aspects to do with styling the maps

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import MapComponent from './Maps/maps'
+import MapComponent from './Maps/Maps'
+
 
 function App() {
 
   return (
     <div>
-        <MapComponent />
+        <MapComponent/>
     </div>
   );
 }
--- src/Maps/maps.jsx
+++ src/Maps/maps.jsx
@@ -1,4 +1,5 @@
-import { APIProvider, Map, Markers } from '@vis.gl/react-google-maps'
+import { APIProvider, Map } from '@vis.gl/react-google-maps'
+import "./Maps.css"
 
 const googleapikey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
@@ -6,20 +7,94 @@ const ubcCoord = { lat: 49.2593, lng: -123.2475};
 
 const sessions = []
 
+const mapStyles = [
+    {
+        "featureType": "administrative",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.school",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.sports_complex",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.medical",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.government",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    }
+];
+
 function MapComponent() {
 
     return (
         <>
             <APIProvider apiKey={googleapikey}>
-            <div style={{ height: "500px", width: "100%" }}>
-                <Map
-                    defaultCenter={ubcCoord}
-                    defaultZoom={14}
-                    disableDefaultUI = "true"
-                />
-            </div>
-            <div>
-            </div>
+                <div id="map">
+                    <Map
+                        defaultCenter={ubcCoord}
+                        defaultZoom={14}
+                        disableDefaultUI = "true"
+                        styles = {mapStyles}
+                    />
+                </div>
             </APIProvider>
         </>
     )
